f4dafd0564114c15760b8cb24439863b773b14c4
Abdul Kabeer <abdulkabeerkhoso082@gmail.com>
2025-09-30 18:34:28 +0500

    Set image shows issue

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -6,7 +6,7 @@ export default function AllProjects() {
       {/* First Card */}
       <div className="w-[80vw] sm:w-lg sm:sm:h-[35%] rounded-[32px] sticky top-[50px] flex items-center justify-center">
         <ProjectCard
-          projectImage="../public/Projects Images/Grocify.png"
+          projectImage="/Projects Images/Grocify.png"
           projectTitle="Grocify- Full Stack Ecommerce Website"
           projectDescription="Created a Full Stack Food Website using (HTML, CSS, JS, and NODE.JS, EXPRESS.JS, MySQL). Performing CRUD operations using relational database. Secure with authentication and middleware using node.js packages."
           githubLink="https://github.com/Abdul-Kabeer-Khoso/Grocery-full-stack-website"
@@ -16,7 +16,7 @@ export default function AllProjects() {
       {/* Second Card */}
       <div className="w-[80vw] sm:w-lg sm:h-[35%] rounded-[32px] sticky top-[80px] flex items-center justify-center">
         <ProjectCard
-          projectImage="../public/Projects Images/Airbnb.png"
+          projectImage="/Projects Images/Airbnb.png"
           projectTitle="Airbnb Full Stack Website"
           projectDescription="Developed a full-stack traveling website (HTML, CSS, JavaScript, Node.js, Express.js, MongoDB) implementing authentication, listing/review CRUD, responsive design (EJS), and cloud image storage, demonstrating MVC and modern web development practices."
           githubLink="https://github.com/Abdul-Kabeer-Khoso/Airbnb-Clone"
@@ -26,7 +26,7 @@ export default function AllProjects() {
       {/* Third Card */}
       <div className="w-[80vw] sm:w-lg sm:h-[35%] rounded-[32px] sticky top-[110px] flex items-center justify-center">
         <ProjectCard
-          projectImage="../public/Projects Images/Music Website.png"
+          projectImage="/Projects Images/Music Website.png"
           projectTitle="Music Website"
           projectDescription="Developed a fully functional Music Player web app using HTML, CSS, and JavaScript. The app features play, pause, next/previous track controls, a progress bar, and responsive UI design for an engaging user experience."
           githubLink=""
@@ -36,7 +36,7 @@ export default function AllProjects() {
       {/* Fourth Card */}
       <div className="w-[80vw] sm:w-lg sm:h-[35%] rounded-[32px] sticky top-[140px] flex items-center justify-center">
         <ProjectCard
-          projectImage="../public/Projects Images/Currency Converter.png"
+          projectImage="/Projects Images/Currency Converter.png"
           projectTitle="Currency Converter Web App"
           projectDescription="Built a Currency Converter web app using HTML, CSS, and JavaScript that allows users to convert between multiple currencies in real time with accurate exchange rates and a clean, responsive interface. Implemented intuitive UI and user-friendly design to ensure smooth and efficient conversions."
           githubLink="https://github.com/Abdul-Kabeer-Khoso/Currency-Converter"
